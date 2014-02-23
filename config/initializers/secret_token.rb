c1dda3fa5e5b2f21ef383739324458713ae5833c
renatodex <renatodex@gmail.com>
2014-02-22 22:00:47 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exploria::Application.config.secret_key_base = '176ddaf9e86b9c1a1fbbd50e1f6ea31c87c3989915d4e0640ef2a5a6a46e80af89297628cd420b52d83f97ff155d720cbce5d904ae455959f1de8f379310c773'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Exploria::Application.config.session_store :cookie_store, key: '_exploria_session'
